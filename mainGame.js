6c63c4b0127324103306b294cb6f946cb396af3b
Shubham <shubham.kakirde@spit.ac.in>
2023-11-26 14:46:27 -0500

    changes for standup

---
+++ mainGame.js
@@ -0,0 +1,22 @@
+// UI Element IDs
+const createGameButtonId = "create-game";
+const joinGameButtonId = "join-game";
+
+// UI elements
+let createGameElement = $(`#${createGameButtonId}`);
+let joinGameElement = $(`#${joinGameButtonId}`);
+
+// Element Event Listeners
+createGameElement.on("click", () => {
+  console.log("create game clicked");
+});
+
+joinGameElement.on("click", () => {
+  console.log("create game clicked");
+});
+
+function initGame() {
+  main();
+}
+
+initGame();
---
+++ wsMain.js
@@ -0,0 +1,37 @@
+import { WebSocketServer } from "ws";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+let app = express();
+
+app.get("/", async (req, res) => {
+  res.send("helo").status(200);
+});
+
+const wsServer = new WebSocketServer({
+  noServer: true,
+  path: "/ws",
+});
+wsServer.on("connection", (socket) => {
+  socket.on("message", (message) => console.log(message));
+});
+
+const server = app.listen(3001);
+server.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
+  });
+});
+
+let wsConnections = [];
+
+wsServer.on("connection", (websocketConnection, connectionRequest) => {
+  console.log("connnnnnn");
+  wsConnections.push(websocketConnection);
+  websocketConnection.on("message", (data) => {
+    data = JSON.parse(data);
+    console.log("recvd: ", JSON.stringify(data));
+    wsConnections.forEach((conn) => conn.send(JSON.stringify(data)));
+  });
+});
